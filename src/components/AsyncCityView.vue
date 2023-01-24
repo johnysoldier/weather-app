853821a547aeebba54a46b5d5fd3d9bbd8478153
johny_s <johny.sldr@gmail.com>
2023-01-24 16:06:18 +0300

    get weather data

---
+++ src/components/AsyncCityView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import axios from "axios";
+import { useRoute } from "vue-router";
+
+const openWeatherAPIKey = "11a0bc415c60d4282d9fc1c5afc35885";
+
+const route = useRoute();
+const getWeatherData = async () => {
+  try {
+    const weatherData = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=${openWeatherAPIKey}&units=metric`
+    );
+
+    return weatherData.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const weatherData = await getWeatherData();
+console.log(weatherData);
+</script>
